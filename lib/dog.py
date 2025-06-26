6ab0fc1909ba1a5556bbaf3c1d39f115fdb4cdbe
Victor Kimathi <vkimathi97@gmail.com>
2025-06-26 14:35:21 +0300

    python-p3-instance-methods

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
+
+fido = Dog()
+fido.bark()
+fido.sit()
+
+snoopy = Dog()
+snoopy.bark()
+snoopy.sit()
